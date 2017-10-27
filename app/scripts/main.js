db2e53b01dfa59f18bda031bf3cd614418aab6d3
Nicholas Luo <nicholasluo@gmail.com>
2017-10-27 10:37:46 +0800

    Convert user search and recent search listing to Angular

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,7 +28,23 @@ var API = {
 
 var githubService = {
     user: function(username, callback) {
-
+        $.getJSON(API.user(username), function(value){
+            var githubUser = {
+                username: value.login,
+                avatar: value.avatar_url,
+                name: value.name,
+                company: value.company,
+                blog: value.blog,
+                location: value.location,
+                email: value.email,
+                hireable: value.hireable,
+                bio: value.bio,
+                repos: value.public_repos,
+                followers: value.followers,
+                joined: value.created_at
+            };
+            callback(githubUser);
+        });
     },
     repos: function(username) {
 
@@ -57,5 +73,66 @@ var App = angular.module('gitberApp', []);
 /**************************
 * Controllers
 **************************/
-App.controller('gitberCtrl', function($scope) {
+App.controller('userSearchCtrl', function($rootScope, $scope) {
+    $scope.username = '';
+    $scope.user = {};
+
+    $scope.loadUser = function(e) {
+        if($scope.username.length > 0) {
+            // Broadcast 'search' event
+            $rootScope.$broadcast('search', $scope.username);
+
+            githubService.user($scope.username, function(githubUser) {
+                if(githubUser) {
+                    $scope.user = githubUser;
+                    $scope.$apply();
+                }
+            });
+        }
+
+        // Prevent form submission
+        if(e){
+            e.preventDefault();
+        }
+    };
+});
+
+App.controller('orgSearchCtrl', function($rootScope, $scope) {
+
+});
+
+App.controller('recentSearchesCtrl', function($rootScope, $scope) {
+    $scope.usernames = [];
+
+    $rootScope.$on('search', function(e, username) {
+        $scope.usernames.push(username);
+    });
+
+    $scope.addUser = function(username){
+        if($scope.usernames.indexOf(username) !== -1) {
+            $scope.removeUser(username);
+        }
+        $scope.usernames.push(username);
+    };
+
+    $scope.removeUser = function(username) {
+        var index = $scope.usernames.indexOf(username);
+        if (index !== -1) {
+            $scope.usernames.splice(index, 1);
+        }
+    };
+
+    $scope.searchAgain = function(username) {
+        // NOOP
+        console.error('not implemented yet');
+    };
+});
+
+/**************************
+ * Filters
+ **************************/
+App.filter('reverse', function() {
+    return function(items) {
+        return items.slice().reverse();
+    };
 });
